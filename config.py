b3b24c132b53b266cd9e3b25b06ef8b7b069cde6
AM <anvar.minulin@gmail.com>
2019-08-21 01:31:41 +0500

    changes

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "Seeeecret key"
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://testuser:testpassword@localhost/testdb'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password@localhost/db'
     SQLALCHEMY_POOL_RECYCLE =86400
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
